834ad912475b5892aa1bc833da190ae5ca964e39
Lakshya Duhoon <142775753+LakshyaDuhoonISU@users.noreply.github.com>
2024-06-14 11:52:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import ToDoList from './components/ToDoList'
-import DigitalClock from './components/DigitalClock'
-import StopWatch from './components/StopWatch'
 
 function App() {
 
 
   return (
     <>
-      {/* <ToDoList /> */}
-      {/* <DigitalClock /> */}
-      <StopWatch />
+      <ToDoList />
     </>
   )
 }
